bd3eae8c3244d64b34ea0088d9893fa6981ab179
bayan-almasri10 <114304152+bayan-almasri10@users.noreply.github.com>
2024-08-04 15:33:55 +0000

    chore: Remove unused files and components from Seller Dashboard

--- src/app/(SELLER)/dashboard/account-settings/page.js
+++
@@ -1,9 +0,0 @@
-"use client"
-import Accountsettings from "@/components/Dashboardseller/Accountsettings"
-export default function AccountSettings () {
-  return (
-    <div> 
-      <Accountsettings/>
-    </div>
-  );
-}
--- src/app/(SELLER)/dashboard/store-settings/page.js
+++
@@ -1,9 +0,0 @@
-"use client"
-import Storesettings from "@/components/Dashboardseller/Storesettings"
-export default function StoreSettings () {
-  return (
-    <div> 
-      <Storesettings/>
-    </div>
-  );
-}
